0e3030d608b8deac632cb2bc4598826a3031d943
ajay <ajay@localhost.localdomain>
2024-10-27 18:18:36 +0530

    files updated

---
+++ src/components/Project.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    <div>
+
+
+<h1>Projects</h1>
+<div>
+    <h2>Project 1</h2>
+    <p>Description 1</p>
+    <a href="https://ajju-chat-app.vercel.app/" target="_blank">Visit Project 1</a>
+</div>
+    </div>
+  )
+}
+
+export default Project
