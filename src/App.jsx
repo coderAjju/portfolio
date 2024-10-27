643506fa9aa3f229e0a094face31890463175e46
ajay <ajay@localhost.localdomain>
2024-10-27 18:19:03 +0530

    files updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Project from './components/Project'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
    <>
    <h1>Ajay Upadhyay</h1>
+   <Project/>
    </>
   )
 }
